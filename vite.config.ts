fee0d8abb8e0f1e8ca2ac5355f82b234dbdf9de2
qiutian <qiutian@chabaidao.com>
2023-05-04 15:15:25 +0800

    feat: 保存项目配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite"
 import uni from "@dcloudio/vite-plugin-uni"
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [uni()]
+  plugins: [uni()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src") // 路径别名配置
+    }
+  }
 })
